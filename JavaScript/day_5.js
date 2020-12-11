bd5ead5fb977dedb7f37feeb1c3a30e13da66414
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-12-11 11:52:58 -0500

    Day 5 Loops

---
+++ JavaScript/day_5.js
@@ -0,0 +1,48 @@
+// Example
+// N = 3
+// The printout should look like this:
+
+// 3 x 1 = 3
+// 3 x 2 = 6
+// 3 x 3 = 9
+// 3 x 4 = 12
+// 3 x 5 = 15
+// 3 x 6 = 18
+// 3 x 7 = 21
+// 3 x 8 = 24
+// 3 x 9 = 27
+// 3 x 10 = 30
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const n = parseInt(readLine(), 10);
+    
+    for (let i = 1; i < 11; i += 1) {
+        console.log(`${n} x ${i} = ${n * i}`);
+    }
+}
---
+++ Python/day_5.py
@@ -0,0 +1,26 @@
+# Example
+# N = 3
+# The printout should look like this:
+
+# 3 x 1 = 3
+# 3 x 2 = 6
+# 3 x 3 = 9
+# 3 x 4 = 12
+# 3 x 5 = 15
+# 3 x 6 = 18
+# 3 x 7 = 21
+# 3 x 8 = 24
+# 3 x 9 = 27
+# 3 x 10 = 30
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input())
+
+    for i in range(1,11):
+        print("{} x {} = {}".format(n, i, (n * i)))
